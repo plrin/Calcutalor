340ec494907219d70a2cc8291f51ce1fa40b973c
Duc Tan Dang <ductan.dang@googlemail.com>
2014-10-10 17:19:37 +0200

    add read me file, add loop function, edit strings

---
+++ readme.txt
@@ -0,0 +1,28 @@
+Dieses Java Programm ist ein Simulator zur Darstellung der internen
+Zahlendarstellung und Berechnung von Computer.
+
+Die Eingabe eines Thermes erfolgt als Post-Fix Notation (umgekehrte polnische
+Notation).
+
+Beispiele:
+3 3 + 				<->		3 + 3
+23 3 4 * 23 - + 	<->		23 + 3 * 4 - 23
+0.3 0.1 - 			<-> 	0.3 - 0.1
+2 31 ^ 1 - 			<->		2^31 - 1
+
+Anmerkungen zur Eingabe:
+Fließkommazhalen können mit einem Punkt[.] beginnen.
+Jede Zahl und jedes Zeichen werden mit einem Leerzeichen getrennt.
+Die Potenzrechnung funktioniert nur mit positiven Ganzen Zahlen.
+Die Schleifen-Funktionen funktiert nur mit Zwei Zahlen
+	Beispiel: 1 0.2 + mit einer Wiederholungsanzahl von 3 ergibt
+			1 0.2 + 0.2 + 0.2 + 0.2 +
+
+
+Probleme die noch behoben werden müssen:
+- die Anzeige der richtigen Binären Berechnung bei Integer zeigt keine 
+	Binärzahlen im Zweikomplement an, sondern stellt bei negativen Zahlen ein
+	Minuszeichen voran
+
+- die Anzeige der richtigen Binären Berechnung bei Floating-Point hat
+	ebenfalls nur 32-Bit zur Darstellung
--- src/Controller2.java
+++ src/Controller2.java
@@ -4,6 +4,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 import javax.swing.JOptionPane;
@@ -28,13 +30,8 @@ public class Controller2 implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			int loops = getLoop();
 			comboBox = getComboBox();
 
-			BigInteger realInt = new BigInteger("0");
-			int compInt = 0;
-			BigDecimal realDec = new BigDecimal("0");
-			float compFloat = 0f;
 			String realOutput = "";
 			String compOutput = "";
 			thermString = "";
@@ -43,40 +40,28 @@ public class Controller2 implements ActionListener {
 			
 			if (comboBox.equals("Integer")) {
 				try {
-					int x = Integer.parseInt(integerRPN());
-					BigInteger y = new BigInteger(bigIntegerRPN());
-					for (int i = 0; i <= loops; i++) {
-						realInt = realInt.add(y);
-						compInt += x;
-					}
-					realOutput = String.valueOf(realInt);
-					compOutput = String.valueOf(compInt);
+					realOutput = String.valueOf(bigIntegerRPN());
+					compOutput = String.valueOf(integerRPN());
 				}
 				catch (Exception exc) {
 					JOptionPane.showMessageDialog(null, "Eingabe ueberschreitet Zahlenbereich\n"
-							+ "[-2^31 = -2.147.483.648 bis 2^31-1 = 2.147.483.647].\n" + e );
+							+ "[-2^31 = -2.147.483.648 bis 2^31-1 = 2.147.483.647].\n" + exc );
 					realOutput = "error";
 					compOutput = "error";
 				}	
 			}
 			else if (comboBox.equals("Float")) {
 				try {
-					float x = Float.parseFloat(floatRPN());
-					BigDecimal y = new BigDecimal(bigDecimalRPN());
-					for (int i = 0; i <= loops; i++) {
-						realDec = realDec.add(y);
-						compFloat += x;
-					}
-					realOutput = String.valueOf(realDec);
-					compOutput = String.valueOf(compFloat);
+					realOutput = String.valueOf(bigDecimalRPN());
+					compOutput = String.valueOf(floatRPN());
 				}
 				catch (Exception exc) {
 					JOptionPane.showMessageDialog(null, "Eingabe ueberschreitet Zahlenbereich"
-							+ "[x nachkommastellen].\n" + e );
+							+ "[x nachkommastellen].\n" + exc );
+					
 					realOutput = "error";
 					compOutput = "error";
-				}	
-				
+				}		
 			}
 			else {
 				JOptionPane.showMessageDialog(null, "Combobox Fehler.");
@@ -101,10 +86,16 @@ public class Controller2 implements ActionListener {
 		Stack<Integer> st = new Stack<Integer>(); 
 		// a and b are stack numbers to pop, x input/result number to push
 		int a = 0, b = 0, x = 0, i; 
-		String [] splitedArray = string.split("\\s");
+		String [] splitArray = string.split("\\s");
 		
-		for (i = 0; i < splitedArray.length; i++) {
-			tk = splitedArray[i];
+		// checking for loops, if true then extend array
+		int loops = getLoop();
+		if (loops > 0) {
+			splitArray = addLoop(splitArray);
+		}
+		
+		for (i = 0; i < splitArray.length; i++) {
+			tk = splitArray[i];
 			if (tk.matches("^[+-]?(\\d+)$")) {
 				x = Integer.parseInt(tk);
 			}
@@ -119,12 +110,13 @@ public class Controller2 implements ActionListener {
 					b = st.pop();
 					if (tk.equals("+")) {
 						x = b + a;
+						
 					}
 					else if (tk.equals("-")) {
 						x = b - a;
 					}
 					else if (tk.equals("*")) {
-							x = b * a;
+						x = b * a;
 					}
 					else if (tk.equals("/")){
 						x = b / a;
@@ -139,14 +131,13 @@ public class Controller2 implements ActionListener {
 					// convert to bin and save in string
 					compString += "   " + twosComplement(b) + " (" + b + ")" + "\n" + tk + " " + 
 					twosComplement(a) + " (" + a + ")" + "\n   --------------------------------\n   " + 
-					twosComplement(x) + " (" + x + ")" + "\n\n";
+					twosComplement(x) + " (" + x + ")" + "\n\n";	
 				}
 			}
 			st.push(x);
-			
 		}
 		
-		if (i < splitedArray.length || st.size() > 1) {
+		if (i < splitArray.length || st.size() > 1) {
 			JOptionPane.showMessageDialog(null, "Rechenzeichen Fehlt.");
 			return "error";
 		}
@@ -164,10 +155,16 @@ public class Controller2 implements ActionListener {
 		boolean isNumber = true;
 		// a and b are stack numbers to pop, x input/result number to push
 		BigInteger a = new BigInteger("0"), b = new BigInteger("0") , x = new BigInteger("0"); 
-		String [] splitedArray = string.split("\\s");
+		String [] splitArray = string.split("\\s");
 		
-		for (i = 0; i < splitedArray.length; i++) {
-			tk = splitedArray[i];
+		// checking for loops, if true then extend array
+		int loops = getLoop();
+		if (loops > 0) {
+			splitArray = addLoop(splitArray);
+		}
+		
+		for (i = 0; i < splitArray.length; i++) {
+			tk = splitArray[i];
 			if (tk.matches("^[+-]?(\\d+)$")) {
 				x = new BigInteger(tk);
 				isNumber = true;
@@ -223,13 +220,14 @@ public class Controller2 implements ActionListener {
 				thermString += tk + "\t\tpush(" + tk + ")\t" + tmp + st.toString() + "\n";  
 			}
 			else {
-				thermString += tmpTk + "\t\t" + b + tmpTk + a + "\n\t\tpush(" + x + ")\t" +
-						tmp + st.toString() + "\t\t" + checkOverflow(x) + "\n";
+				thermString += tmpTk + "\t\t" + "pop(" + b + "," + a + ")\n\t\t" + b + tmpTk + 
+						a + "\n\t\tpush(" + x + ")\t" + tmp + st.toString() + 
+						"\t\t" + checkOverflow(x) + "\n";
 			}
 			// end of string save
 		}
 		
-		if (i < splitedArray.length || st.size() > 1) {
+		if (i < splitArray.length || st.size() > 1) {
 			JOptionPane.showMessageDialog(null, "Rechenzeichen Fehlt.");
 			return "error";
 		}
@@ -249,12 +247,18 @@ public class Controller2 implements ActionListener {
 		String tk;
 		Stack<Float> st = new Stack<Float>(); 
 		// a and b are stack numbers to pop, x input/result number to push
-		float a = 0, b = 0, x = 0;
+		float a = 0f, b = 0f, x = 0f;
 		int i; 
-		String [] splitedArray = string.split("\\s");
+		String [] splitArray = string.split("\\s");
 		
-		for (i = 0; i < splitedArray.length; i++) {
-			tk = splitedArray[i];
+		// checking for loops, if true then extend array
+		int loops = getLoop();
+		if (loops > 0) {
+			splitArray = addLoop(splitArray);
+		}
+		
+		for (i = 0; i < splitArray.length; i++) {
+			tk = splitArray[i];
 			if (tk.matches("^[+-]?(\\.\\d+|\\d+(\\.\\d+)?)$")) {
 				x = Float.parseFloat(tk);
 			}
@@ -280,14 +284,8 @@ public class Controller2 implements ActionListener {
 						x = b / a;
 					}
 					else if (tk.equals("^")) {
-						String strA = Float.toString(a);
-						double dA = Double.parseDouble(strA);
-						String strB = Float.toString(b);
-						double dB = Double.parseDouble(strB);
-						double dX = Math.pow(dB, dA);
-						String s = Double.toString(dX);
-						x = Float.valueOf(s);
-						System.out.println("double: " + dX + " string: " + s + " float: " + x);
+						x = floatPow(b,a);
+						
 					}
 					else {
 						JOptionPane.showMessageDialog(null, "Fehler.");
@@ -303,7 +301,7 @@ public class Controller2 implements ActionListener {
 			
 		}
 		
-		if (i < splitedArray.length || st.size() > 1) {
+		if (i < splitArray.length || st.size() > 1) {
 			JOptionPane.showMessageDialog(null, "Rechenzeichen Fehlt.");
 			return "error";
 		}
@@ -322,10 +320,16 @@ public class Controller2 implements ActionListener {
 		boolean isNumber = true;
 		// a and b are stack numbers to pop, x input/result number to push
 		BigDecimal a = new BigDecimal("0"), b = new BigDecimal("0") , x = new BigDecimal("0.2"); 
-		String [] splitedArray = string.split("\\s");
+		String [] splitArray = string.split("\\s");
 		
-		for (i = 0; i < splitedArray.length; i++) {
-			tk = splitedArray[i];
+		// checking for loops, if true then extend array
+		int loops = getLoop();
+		if (loops > 0) {
+			splitArray = addLoop(splitArray);
+		}		
+		
+		for (i = 0; i < splitArray.length; i++) {
+			tk = splitArray[i];
 			if (tk.matches("^[+-]?(\\.\\d+|\\d+(\\.\\d+)?)$")) {
 				x = new BigDecimal(tk);
 				isNumber = true;
@@ -354,6 +358,7 @@ public class Controller2 implements ActionListener {
 					else if (tk.equals("^")) {
 						int an = a.intValue();
 						x = b.pow(an);
+						System.out.print(x);
 					}
 					else {
 						JOptionPane.showMessageDialog(null, "Fehler.");
@@ -379,13 +384,14 @@ public class Controller2 implements ActionListener {
 				thermString += tk + "\t\tpush(" + tk + ")\t" + tmp + st.toString() + "\n";  
 			}
 			else {
-				thermString += tmpTk + "\t\t" + b + tmpTk + a + "\n\t\tpush(" + x + ")\t" +
+				thermString += tmpTk + "\t\t" + "pop(" + b + "," + a + ")\n\t\t" + b + 
+						tmpTk + a + "\n\t\tpush(" + x + ")\t" +
 						tmp + st.toString() + "\t\t" + "" + "\n";
 			}
 			// end of string save
 		}
 		
-		if (i < splitedArray.length || st.size() > 1) {
+		if (i < splitArray.length || st.size() > 1) {
 			JOptionPane.showMessageDialog(null, "Rechenzeichen Fehlt.");
 			return "error";
 		}
@@ -394,7 +400,7 @@ public class Controller2 implements ActionListener {
 		}
 	}
 	
-	/* end of float and big float calculation */
+	/* end of float and big decimal calculation */
 	
 	/* --------------------------------------------- */
 	
@@ -456,6 +462,25 @@ public class Controller2 implements ActionListener {
 		return res;
 	}
 	
+	/* exponential function for float with non floating point exponent */
+	public static float floatPow(float base, float exp) {
+		float res = 1;
+		if (exp == 0) {
+			res = 0;
+		}
+		else if (exp == 1) {
+			res = base;
+		}
+		// result would be decimal between 0 and 1
+		for (int i = 0; i < Math.abs(exp); i++) {
+			res *= base;
+		}
+		if (exp < 0) {
+			res = 1 / res;
+		}
+		
+		return res;
+	}
 	
 	/* convert an integer to a two's Complement with 32 Bits, each digit will be shown */
 	public static String twosComplement(int i) {
@@ -476,7 +501,9 @@ public class Controller2 implements ActionListener {
 	/* convert float to binary representation IEEE 754 */
 	public static String float2bin(float f) {
 		String str = new String("");
-		str = Integer.toString(Float.floatToIntBits(f), 2);
+		//str = Integer.toString(Float.floatToIntBits(f), 2);
+		System.out.println(str);
+		str = String.format("%32s", Integer.toBinaryString(Float.floatToIntBits(f))).replace(' ', '0');
 		str = str.substring(0, 1) + " " + str.substring(1, 9) + " " + str.substring(9, str.length());
 		
 		return str;
@@ -513,4 +540,17 @@ public class Controller2 implements ActionListener {
 		return compStart + compString;
 	}	
 
+	
+	/* extend array to the number of loops */
+	public String [] addLoop(String [] array) {
+		ArrayList<String> list = new ArrayList<String>(Arrays.asList(array));
+		String number = array[1];
+		String token = array[2];
+		for (int i = 1; i <= getLoop(); i++) {
+			list.add(1, number);
+			list.add(2, token);
+		}
+		array = list.toArray(array);
+		return array;
+	}
 }
